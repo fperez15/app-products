9b2df35641d6debfc54bd5032a3975be0737c6bc
fperez15 <francis.perez1590@gmail.com>
2025-03-04 12:27:55 -0500

    feat: se crearon los servicios e interfaz de los productos

---
+++ src/services/product.ts
@@ -0,0 +1,59 @@
+import { Product } from "@/types/product";
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/products";
+
+const handleApiError = (error: unknown): never => {
+  if (axios.isAxiosError(error)) {
+    if (process.env.NODE_ENV !== "production") {
+      console.error("Error en la API:", error.response?.data || error.message);
+    }
+    throw new Error(error.response?.data?.message || "Error en la API");
+  }
+  throw new Error("Error inesperado");
+};
+
+export const getProducts = async (): Promise<Product[]> => {
+  try {
+    const response = await axios.get<Product[]>(API_URL);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const getProductById = async (id: number): Promise<Product> => {
+  try {
+    const response = await axios.get<Product>(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const addProduct = async (product: Product): Promise<Product> => {
+  try {
+    const response = await axios.post<Product>(API_URL, product);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const updateProduct = async (id: number, product: Partial<Product>): Promise<Product> => {
+  try {
+    const response = await axios.put<Product>(`${API_URL}/${id}`, product);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const deleteProduct = async (id: number): Promise<{ message: string }> => {
+  try {
+    axios.delete(`${API_URL}/${id}`);
+    return { message: "Producto eliminado correctamente." };
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+
+
+export interface Product {
+    sku: number,
+    name: string,
+    category?: string,
+    brand: string,
+    price: number,
+    stock: number,
+    rating: number,
+    image: string
+}
