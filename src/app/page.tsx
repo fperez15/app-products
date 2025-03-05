7da6b54ff3c23d12a8aabb0db4277db32ac45c47
fperez15 <francis.perez1590@gmail.com>
2025-03-05 00:32:51 -0500

    fix: se corrigio los componentes SkeletonLoader, SkeletonCard y ProductList implementando SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import ProductList from "@/components/ProductList";
+import SkeletonLoader from "@/components/SkeletonLoader";
+import { getProducts } from "@/services/product";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <>
     <div className="container mx-auto px-4 py-8">
-      <ProductList />
+      <Suspense fallback={<SkeletonLoader />}>
+        <ProductListWrapper />
+      </Suspense>
     </div>
-    </>
   );
+}
+
+async function ProductListWrapper() {
+  const initialProducts = await getProducts();
+  return <ProductList initialProducts={initialProducts} />;
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,19 +1,13 @@
-"use client"
 
-import { useProducts } from '@/hooks/useProducts'
-import React, { useState } from 'react'
 import ProductCard from './ProductCard';
-import Skeleton from './SkeletonLoader';
+import { Product } from '@/types/product';
 
-export default function ProductList() {
+interface ProductListProps {
+  initialProducts: Product[];
+}
 
-    const { data: products, isLoading, error} = useProducts();
-    const [loading, setLoading] = useState(true);
-    
-    console.log("Productos en ProductList:", products);
+export default function ProductList({ initialProducts }: ProductListProps) {
 
-    if (isLoading || loading) return <Skeleton onComplete={() => setLoading(false)} />;
-    if (error) return <p className='text-center text-red-500'>❌ Error al cargar los productos</p>
   return (
     <>
     <div className='bg-white'>
@@ -21,10 +15,13 @@ export default function ProductList() {
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Nuestros Productos</h2>
 
             <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products?.map((product) => (
+            {initialProducts?.map((product) => (
                 <ProductCard key={product.sku} product={product} />
             ))}
             </div>
+            {initialProducts.length === 0 && (
+          <p className="text-center text-gray-500">No se encontraron productos.</p>
+        )}
         </div>
     </div>
 
