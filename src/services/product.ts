413915fa076d4c5c4d98e445b58ef0b8d90e1027
fperez15 <francis.perez1590@gmail.com>
2025-03-05 00:30:25 -0500

    fix: se mejoro getProducts() aplicando el SSR

--- src/services/product.ts
+++ src/services/product.ts
@@ -13,13 +13,12 @@ const handleApiError = (error: unknown): never => {
   throw new Error("Error inesperado");
 };
 
+
 export const getProducts = async (): Promise<Product[]> => {
-  try {
-    const response = await axios.get<Product[]>(API_URL);
-    return response.data;
-  } catch (error) {
-    return handleApiError(error);
-  }
+  await new Promise(resolve => setTimeout(resolve, 1000)); // se agrega para mostrar el SkeletonLoader 
+  const response = await fetch(API_URL, { cache: "no-store" });
+  if (!response.ok) throw new Error("Error al obtener los productos");
+  return response.json();
 };
 
 export const getProductById = async (id: number): Promise<Product> => {
@@ -40,20 +39,20 @@ export const addProduct = async (product: Product): Promise<Product> => {
   }
 };
 
-export const updateProduct = async (id: number, product: Partial<Product>): Promise<Product> => {
-  try {
-    const response = await axios.put<Product>(`${API_URL}/${id}`, product);
-    return response.data;
-  } catch (error) {
-    return handleApiError(error);
-  }
-};
+// export const updateProduct = async (id: number, product: Partial<Product>): Promise<Product> => {
+//   try {
+//     const response = await axios.put<Product>(`${API_URL}/${id}`, product);
+//     return response.data;
+//   } catch (error) {
+//     return handleApiError(error);
+//   }
+// };
 
-export const deleteProduct = async (id: number): Promise<{ message: string }> => {
-  try {
-    axios.delete(`${API_URL}/${id}`);
-    return { message: "Producto eliminado correctamente." };
-  } catch (error) {
-    return handleApiError(error);
-  }
-};
+// export const deleteProduct = async (id: number): Promise<{ message: string }> => {
+//   try {
+//     axios.delete(`${API_URL}/${id}`);
+//     return { message: "Producto eliminado correctamente." };
+//   } catch (error) {
+//     return handleApiError(error);
+//   }
+// };
